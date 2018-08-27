66d183c2ad3a58bcabd44921f63961e38526164e
HeesuShin <shinhs2009@gmail.com>
2018-08-28 03:46:42 +0900

    Update Mortal.sol

--- contracts/Mortal.sol
+++ contracts/Mortal.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.4.24;
 
 import "./Ownable.sol";
 
+/** @title Mortal */
 contract Mortal is Ownable {
+    /** @dev selfdestruct contract in emergency */
     function kill() public onlyOwner {
         selfdestruct(owner);
     }
